99ee25b63f4c3580a08039a8fabc8f714838b3bb
Ryan Weaver <ryan@thatsquality.com>
2015-05-03 13:25:54 -0400

    Delete AppBundle.php

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,6 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
-            new App\AppBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
