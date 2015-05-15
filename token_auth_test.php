8508d162ecda642edf62f9654201de1bedb1c253
Ryan Weaver <ryan@thatsquality.com>
2015-05-15 10:53:52 -0700

    Adding a way to test the authentication

---
+++ token_auth_test.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Token\Auth\Test;
+
+require __DIR__.'/vendor/autoload.php';
+
+use GuzzleHttp\Client;
+
+$client = new Client([
+    'base_url' => 'http://localhost:9004',
+    'defaults' => [
+        'exceptions' => false,
+    ]
+]);
+
+$response = $client->get('/admin/post/', [
+    'headers' => [
+        'X-Auth-Token' => 'anna_admin'
+    ]
+]);
+
+echo $response."\n\n";
