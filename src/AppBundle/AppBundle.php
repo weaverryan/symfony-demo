56e8a056a47bd97583f839cd54912aa69118fe74
Ryan Weaver <ryan@thatsquality.com>
2015-05-25 19:50:58 -0400

    removing manual registration of guard

--- src/AppBundle/AppBundle.php
+++ src/AppBundle/AppBundle.php
@@ -37,12 +37,4 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class AppBundle extends Bundle
 {
-    public function build(ContainerBuilder $container)
-    {
-        parent::build($container);
-
-        $extension = $container->getExtension('security');
-        $extension->addSecurityListenerFactory(new GuardAuthenticationFactory());
-    }
-
 }
